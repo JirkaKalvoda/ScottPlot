81be0fe881421ed6a5fc6b24400d2797bf9c2ef1
JirkaKalvoda <pzkpfw_5@163.com>
2023-02-24 11:24:09 +0800

    增加枚举描述字符串用于显示

--- src/ScottPlot4/ScottPlot/Enums/LineStyle.cs
+++ src/ScottPlot4/ScottPlot/Enums/LineStyle.cs
@@ -1,12 +1,25 @@
-﻿namespace ScottPlot
+﻿using System.ComponentModel;
+
+namespace ScottPlot
 {
     public enum LineStyle
     {
+        [Description("None")]
         None,
+
+        [Description("Solid")]
         Solid,
+
+        [Description("Dash")]
         Dash,
+
+        [Description("Dash Dot")]
         DashDot,
+
+        [Description("Dash Dot Dot")]
         DashDotDot,
+
+        [Description("Dot")]
         Dot
     }
 }
--- src/ScottPlot4/ScottPlot/Marker/MarkerShape.cs
+++ src/ScottPlot4/ScottPlot/Marker/MarkerShape.cs
@@ -1,24 +1,61 @@
-﻿namespace ScottPlot
+﻿using System.ComponentModel;
+
+namespace ScottPlot
 {
     public enum MarkerShape
     {
+        [Description("None")]
         none,
+
+        [Description("Filled Circle")]
         filledCircle,
+
+        [Description("Filled Square")]
         filledSquare,
+
+        [Description("Open Circle")]
         openCircle,
+
+        [Description("Open Square")]
         openSquare,
+
+        [Description("Filled Diamond")]
         filledDiamond,
+
+        [Description("Open Diamond")]
         openDiamond,
+
+        [Description("Asterisk")]
         asterisk,
+
+        [Description("Hash Tag")]
         hashTag,
+
+        [Description("Cross")]
         cross,
+
+        [Description("Eks")]
         eks,
+
+        [Description("Vertical Bar")]
         verticalBar,
+
+        [Description("Tri Up")]
         triUp,
+
+        [Description("Tri Down")]
         triDown,
+
+        [Description("Filled Triangle Up")]
         filledTriangleUp,
+
+        [Description("Filled Triangle Down")]
         filledTriangleDown,
+
+        [Description("Open Triangle Up")]
         openTriangleUp,
+
+        [Description("Open Triangle Down")]
         openTriangleDown,
     }
 }
